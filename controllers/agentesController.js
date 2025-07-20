08e4be6230a7e8bb8eed2c961855630ad02acf6c
Marco Fabian <marcofabianufmg@hotmail.com>
2025-07-20 17:14:03 -0300

    feat: controladores implementados

---
+++ controllers/agentesController.js
@@ -0,0 +1,185 @@
+const agentesRepository = require('../repositories/agentesRepository');
+const { createValidationError, createNotFoundError, validateRequiredFields, validateDateFormat, validateUUID } = require('../utils/errorHandler');
+
+function getAllAgentes(req, res, next) {
+    try {
+        const { cargo, sort } = req.query;
+        let agentes;
+
+        if (cargo) {
+            agentes = agentesRepository.findByCargo(cargo);
+        } else if (sort) {
+            const order = sort.startsWith('-') ? 'desc' : 'asc';
+            const field = sort.replace('-', '');
+            if (field === 'dataDeIncorporacao') {
+                agentes = agentesRepository.findAllSorted(order);
+            } else {
+                agentes = agentesRepository.findAll();
+            }
+        } else {
+            agentes = agentesRepository.findAll();
+        }
+
+        res.status(200).json(agentes);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function getAgenteById(req, res, next) {
+    try {
+        const { id } = req.params;
+
+        if (!validateUUID(id)) {
+            throw createValidationError('Parâmetros inválidos', { id: 'ID deve ser um UUID válido' });
+        }
+
+        const agente = agentesRepository.findById(id);
+        if (!agente) {
+            throw createNotFoundError('Agente não encontrado');
+        }
+
+        res.status(200).json(agente);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function createAgente(req, res, next) {
+    try {
+        const dados = req.body;
+        const requiredFields = ['nome', 'dataDeIncorporacao', 'cargo'];
+        
+        const validationErrors = validateRequiredFields(dados, requiredFields);
+        const errors = {};
+
+        if (validationErrors) {
+            Object.assign(errors, validationErrors);
+        }
+
+        if (dados.dataDeIncorporacao) {
+            const dateError = validateDateFormat(dados.dataDeIncorporacao, 'dataDeIncorporacao');
+            if (dateError) {
+                errors.dataDeIncorporacao = dateError;
+            }
+        }
+
+        const validCargos = ['inspetor', 'delegado'];
+        if (dados.cargo && !validCargos.includes(dados.cargo)) {
+            errors.cargo = "O campo 'cargo' deve ser 'inspetor' ou 'delegado'";
+        }
+
+        if (Object.keys(errors).length > 0) {
+            throw createValidationError('Parâmetros inválidos', errors);
+        }
+
+        const novoAgente = agentesRepository.create(dados);
+        res.status(201).json(novoAgente);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function updateAgente(req, res, next) {
+    try {
+        const { id } = req.params;
+        const dados = req.body;
+
+        if (!validateUUID(id)) {
+            throw createValidationError('Parâmetros inválidos', { id: 'ID deve ser um UUID válido' });
+        }
+
+        const errors = {};
+
+        if (dados.dataDeIncorporacao) {
+            const dateError = validateDateFormat(dados.dataDeIncorporacao, 'dataDeIncorporacao');
+            if (dateError) {
+                errors.dataDeIncorporacao = dateError;
+            }
+        }
+
+        const validCargos = ['inspetor', 'delegado'];
+        if (dados.cargo && !validCargos.includes(dados.cargo)) {
+            errors.cargo = "O campo 'cargo' deve ser 'inspetor' ou 'delegado'";
+        }
+
+        if (Object.keys(errors).length > 0) {
+            throw createValidationError('Parâmetros inválidos', errors);
+        }
+
+        const agenteAtualizado = agentesRepository.updateById(id, dados);
+        if (!agenteAtualizado) {
+            throw createNotFoundError('Agente não encontrado');
+        }
+
+        res.status(200).json(agenteAtualizado);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function patchAgente(req, res, next) {
+    try {
+        const { id } = req.params;
+        const dados = req.body;
+
+        if (!validateUUID(id)) {
+            throw createValidationError('Parâmetros inválidos', { id: 'ID deve ser um UUID válido' });
+        }
+
+        const errors = {};
+
+        if (dados.dataDeIncorporacao) {
+            const dateError = validateDateFormat(dados.dataDeIncorporacao, 'dataDeIncorporacao');
+            if (dateError) {
+                errors.dataDeIncorporacao = dateError;
+            }
+        }
+
+        const validCargos = ['inspetor', 'delegado'];
+        if (dados.cargo && !validCargos.includes(dados.cargo)) {
+            errors.cargo = "O campo 'cargo' deve ser 'inspetor' ou 'delegado'";
+        }
+
+        if (Object.keys(errors).length > 0) {
+            throw createValidationError('Parâmetros inválidos', errors);
+        }
+
+        const agenteAtualizado = agentesRepository.updateById(id, dados);
+        if (!agenteAtualizado) {
+            throw createNotFoundError('Agente não encontrado');
+        }
+
+        res.status(200).json(agenteAtualizado);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function deleteAgente(req, res, next) {
+    try {
+        const { id } = req.params;
+
+        if (!validateUUID(id)) {
+            throw createValidationError('Parâmetros inválidos', { id: 'ID deve ser um UUID válido' });
+        }
+
+        const deleted = agentesRepository.deleteById(id);
+        if (!deleted) {
+            throw createNotFoundError('Agente não encontrado');
+        }
+
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    getAllAgentes,
+    getAgenteById,
+    createAgente,
+    updateAgente,
+    patchAgente,
+    deleteAgente
+}; 
---
+++ controllers/casosController.js
@@ -0,0 +1,228 @@
+const casosRepository = require('../repositories/casosRepository');
+const agentesRepository = require('../repositories/agentesRepository');
+const { createValidationError, createNotFoundError, validateRequiredFields, validateCasoStatus, validateUUID } = require('../utils/errorHandler');
+
+function getAllCasos(req, res, next) {
+    try {
+        const { agente_id, status, q } = req.query;
+        let casos;
+
+        if (agente_id) {
+            if (!validateUUID(agente_id)) {
+                throw createValidationError('Parâmetros inválidos', { agente_id: 'agente_id deve ser um UUID válido' });
+            }
+            casos = casosRepository.findByAgenteId(agente_id);
+        } else if (status) {
+            casos = casosRepository.findByStatus(status);
+        } else if (q) {
+            casos = casosRepository.search(q);
+        } else {
+            casos = casosRepository.findAll();
+        }
+
+        res.status(200).json(casos);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function getCasoById(req, res, next) {
+    try {
+        const { id } = req.params;
+
+        if (!validateUUID(id)) {
+            throw createValidationError('Parâmetros inválidos', { id: 'ID deve ser um UUID válido' });
+        }
+
+        const caso = casosRepository.findById(id);
+        if (!caso) {
+            throw createNotFoundError('Caso não encontrado');
+        }
+
+        res.status(200).json(caso);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function getAgenteFromCaso(req, res, next) {
+    try {
+        const { caso_id } = req.params;
+
+        if (!validateUUID(caso_id)) {
+            throw createValidationError('Parâmetros inválidos', { caso_id: 'caso_id deve ser um UUID válido' });
+        }
+
+        const caso = casosRepository.findById(caso_id);
+        if (!caso) {
+            throw createNotFoundError('Caso não encontrado');
+        }
+
+        const agente = agentesRepository.findById(caso.agente_id);
+        if (!agente) {
+            throw createNotFoundError('Agente responsável não encontrado');
+        }
+
+        res.status(200).json(agente);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function createCaso(req, res, next) {
+    try {
+        const dados = req.body;
+        const requiredFields = ['titulo', 'descricao', 'status', 'agente_id'];
+        
+        const validationErrors = validateRequiredFields(dados, requiredFields);
+        const errors = {};
+
+        if (validationErrors) {
+            Object.assign(errors, validationErrors);
+        }
+
+        if (dados.status) {
+            const statusError = validateCasoStatus(dados.status);
+            if (statusError) {
+                errors.status = statusError;
+            }
+        }
+
+        if (dados.agente_id) {
+            if (!validateUUID(dados.agente_id)) {
+                errors.agente_id = 'agente_id deve ser um UUID válido';
+            } else {
+                const agente = agentesRepository.findById(dados.agente_id);
+                if (!agente) {
+                    errors.agente_id = 'Agente não encontrado';
+                }
+            }
+        }
+
+        if (Object.keys(errors).length > 0) {
+            throw createValidationError('Parâmetros inválidos', errors);
+        }
+
+        const novoCaso = casosRepository.create(dados);
+        res.status(201).json(novoCaso);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function updateCaso(req, res, next) {
+    try {
+        const { id } = req.params;
+        const dados = req.body;
+
+        if (!validateUUID(id)) {
+            throw createValidationError('Parâmetros inválidos', { id: 'ID deve ser um UUID válido' });
+        }
+
+        const errors = {};
+
+        if (dados.status) {
+            const statusError = validateCasoStatus(dados.status);
+            if (statusError) {
+                errors.status = statusError;
+            }
+        }
+
+        if (dados.agente_id) {
+            if (!validateUUID(dados.agente_id)) {
+                errors.agente_id = 'agente_id deve ser um UUID válido';
+            } else {
+                const agente = agentesRepository.findById(dados.agente_id);
+                if (!agente) {
+                    errors.agente_id = 'Agente não encontrado';
+                }
+            }
+        }
+
+        if (Object.keys(errors).length > 0) {
+            throw createValidationError('Parâmetros inválidos', errors);
+        }
+
+        const casoAtualizado = casosRepository.updateById(id, dados);
+        if (!casoAtualizado) {
+            throw createNotFoundError('Caso não encontrado');
+        }
+
+        res.status(200).json(casoAtualizado);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function patchCaso(req, res, next) {
+    try {
+        const { id } = req.params;
+        const dados = req.body;
+
+        if (!validateUUID(id)) {
+            throw createValidationError('Parâmetros inválidos', { id: 'ID deve ser um UUID válido' });
+        }
+
+        const errors = {};
+
+        if (dados.status) {
+            const statusError = validateCasoStatus(dados.status);
+            if (statusError) {
+                errors.status = statusError;
+            }
+        }
+
+        if (dados.agente_id) {
+            if (!validateUUID(dados.agente_id)) {
+                errors.agente_id = 'agente_id deve ser um UUID válido';
+            } else {
+                const agente = agentesRepository.findById(dados.agente_id);
+                if (!agente) {
+                    errors.agente_id = 'Agente não encontrado';
+                }
+            }
+        }
+
+        if (Object.keys(errors).length > 0) {
+            throw createValidationError('Parâmetros inválidos', errors);
+        }
+
+        const casoAtualizado = casosRepository.updateById(id, dados);
+        if (!casoAtualizado) {
+            throw createNotFoundError('Caso não encontrado');
+        }
+
+        res.status(200).json(casoAtualizado);
+    } catch (error) {
+        next(error);
+    }
+}
+
+function deleteCaso(req, res, next) {
+    try {
+        const { id } = req.params;
+
+        if (!validateUUID(id)) {
+            throw createValidationError('Parâmetros inválidos', { id: 'ID deve ser um UUID válido' });
+        }
+
+        const deleted = casosRepository.deleteById(id);
+        if (!deleted) {
+            throw createNotFoundError('Caso não encontrado');
+        }
+
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    getAllCasos,
+    getCasoById,
+    getAgenteFromCaso,
+    createCaso,
+    updateCaso,
+    patchCaso,
+    deleteCaso
+}; 
